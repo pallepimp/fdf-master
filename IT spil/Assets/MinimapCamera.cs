b80e0d2ba6e82e72f9e5c120bb238f753466d906
Vittermand <vitusbt@gmail.com>
2017-05-16 08:37:39 +0200

    Stuff
    sdfg

---
+++ IT spil/Assets/MinimapCamera.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MinimapCamera : MonoBehaviour {
+
+    GameObject target;
+    
+    // Use this for initialization
+	void Start () {
+        target = GameObject.FindWithTag("Player");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        gameObject.transform.position = new Vector3(target.transform.position.x, 1000, target.transform.position.z);
+        gameObject.transform.eulerAngles = new Vector3(90, target.transform.eulerAngles.y, 0);
+    }
+}
